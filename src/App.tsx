97e8a2143b1c9898130895f64f9ba35510fcb9fe
Gabriel Dias <gabrieldiascorrea7@gmail.com>
2022-11-16 16:31:58 -0300

    initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,10 @@
-import { useEffect } from "react"
+
 
 function App() {
 
-	useEffect(() => {
-
-		async function listar() {
-			const response = await fetch("https://api-braga.herokuapp.com/api/barbersAll/4")
-			const data = await response.json()
-			console.log(data)
-		}
-
-		listar()
-
-	}, [])
-
 	return (
 		<>
-			
+			oi
 		</>
 	)
 }
