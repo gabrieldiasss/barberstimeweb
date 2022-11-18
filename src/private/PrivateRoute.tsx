d6edd1306a55124bba3ecd73670ab0770ef82e8a
Gabriel Dias <gabrieldiascorrea7@gmail.com>
2022-11-18 17:17:32 -0300

    redirect to main route

--- src/private/PrivateRoute.tsx
+++ src/private/PrivateRoute.tsx
@@ -2,7 +2,7 @@ import { Redirect, Route } from 'react-router-dom'
 
 function Private(props: any) {
     const isLogged = !!localStorage.getItem("accessToken")
-    return isLogged ? <Route {...props} /> : <Redirect to="/login" />
+    return isLogged ? <Route {...props} /> : <Redirect to="/" />
 }
 
 export default Private
